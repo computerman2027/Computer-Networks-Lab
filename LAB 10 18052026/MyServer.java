f4e850836edf4a85960bad6347837155cd3d8752
computerman2027 <imonmallik3@gmail.com>
2026-05-20 11:30:27 +0530

    server code created

---
+++ LAB 10 18052026/MyServer.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.*;
+
+public class MyServer {
+
+    public static void main(String[] args) {
+        try{
+            ServerSocket ss = new ServerSocket(6666);
+
+            Socket s = ss.accept();
+
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+
+            String str = (String)dis.readUTF();
+
+            System.out.printf("Message = %s\n",dis);
+
+            ss.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
